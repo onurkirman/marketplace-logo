24fb4d6c14164271ddf68e5d06654a8c4b1274c3
onurkirman <onur.kirman@ozu.edu.tr>
2019-06-28 14:14:09 +0300

    JUnit Tests added

--- test/java/restAPI/MainTest.java
+++ test/java/restAPI/MainTest.java
@@ -1,6 +1,8 @@
 package restAPI;
 
+import ch.qos.logback.core.encoder.EchoEncoder;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import restAPI.Model.Auction;
+import restAPI.Model.Buyer;
+import restAPI.Model.Lot;
+import restAPI.Model.Seller;
+import restAPI.Service.LotService;
+import restAPI.Service.MarketplaceService;
 
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -22,15 +30,22 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class MainTest {
 
     @Autowired
-    private MockMvc mockMvc;
+    MarketplaceService  MS;
+    @Autowired
+    LotService          LS;
+    @Autowired
+    private MockMvc     mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    public void init () throws Exception {
+    public void initUsingJsonRequest () throws Exception {
         this.mockMvc.perform(get("/init")).andDo(print()).andExpect(status().isOk());
+        MS.clearAll();
     }
 
     @Test
-    public void addingSeller () throws Exception {
+    public void addingSellerUsingJson () throws Exception {
         Object randomObj = new Object() {
             public final String sellerName = "Test Seller";
         };
@@ -39,42 +54,125 @@ public class MainTest {
         String json = objectMapper.writeValueAsString(randomObj);
 
         this.mockMvc.perform(post("/seller")
-                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .content(json))
-                .andDo(print()).andExpect(status().isOk());
+                    .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                    .content(json))
+                    .andDo(print()).andExpect(status().isOk());
+        MS.clearAll();
     }
 
     @Test
-    public void addingBuyer () throws Exception {
+    public void addingBuyerUsingJson () throws Exception {
         Object randomObj = new Object() {
             public final String buyerName = "Test Buyer";
         };
 
-        ObjectMapper objectMapper = new ObjectMapper();
         String json = objectMapper.writeValueAsString(randomObj);
 
         this.mockMvc.perform(post("/buyer")
-                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
-                .content(json))
-                .andDo(print()).andExpect(status().isOk());
-    }
-
-    /*    @Test
-        public void addingAuction () throws Exception {
-            Object randomObj = new Object() {
-                public final String sellerName  = "Test Auction";
-                public final String auctionDay  = "10 10 1010";
-                public final String price       = "5.5";
-                public final String duration    = "2";
-            };
-
-            ObjectMapper objectMapper = new ObjectMapper();
-            String json = objectMapper.writeValueAsString(randomObj);
-
-            this.mockMvc.perform(post("/auction/600/1")
                     .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
                     .content(json))
                     .andDo(print()).andExpect(status().isOk());
-        }
-*/
+        MS.clearAll();
+    }
+
+
+    @Test
+    public void addLotWithNegativeWeight () throws Exception {
+        Seller seller = new Seller("TestSeller");
+        MS.addSeller(seller);
+        seller.setID("1111");
+
+        Object randomObj = new Object() {
+            public final String cultivar = "Test Banana";
+            public final String origin = "Turkey";
+            public final String harvestDate= "12 June 2019";
+            public final String weight = "-100";
+        };
+
+        String json = objectMapper.writeValueAsString(randomObj);
+
+        this.mockMvc.perform(post("/addLot/1111")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(json))
+                .andDo(print()).andExpect(status().isForbidden());
+        MS.clearAll();
+    }
+
+    @Test
+    public void addLotWithNotSufficientWeight () throws Exception {
+        Seller seller = new Seller("TestSeller");
+        MS.addSeller(seller);
+        seller.setID("1111");
+
+        Object randomObj = new Object() {
+            public final String cultivar = "Test Banana";
+            public final String origin = "Turkey";
+            public final String harvestDate= "12 June 2019";
+            public final String weight = "200";
+        };
+
+        String json = objectMapper.writeValueAsString(randomObj);
+
+        this.mockMvc.perform(post("/addLot/1111")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(json))
+                .andDo(print()).andExpect(status().isForbidden());
+        MS.clearAll();
+    }
+
+    @Test
+    public void addLotWithNotExistingSeller () throws Exception {
+        Object randomObj = new Object() {
+            public final String cultivar = "Test Banana";
+            public final String origin = "Turkey";
+            public final String harvestDate= "12 June 2019";
+            public final String weight = "200";
+        };
+
+        String json = objectMapper.writeValueAsString(randomObj);
+
+        this.mockMvc.perform(post("/addLot/-100")
+                .contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)
+                .content(json))
+                .andDo(print()).andExpect(status().isNotFound());
+        MS.clearAll();
+    }
+
+    @Test
+    public void addingBuyerUsingObject () {
+        Buyer buyerInserted = new Buyer("TestBuyer");
+        MS.addBuyer(buyerInserted);
+        buyerInserted.setID("1111");
+        Buyer buyerGathered = MS.getBuyer("1111");
+        assertEquals(buyerInserted,buyerGathered);
+        MS.clearAll();
+    }
+
+    @Test
+    public void addingSellerUsingObject () {
+        Seller sellerInserted = new Seller("TestBuyer");
+        MS.addSeller(sellerInserted);
+        sellerInserted.setID("1111");
+        Seller sellerGathered = MS.getSeller("1111");
+        assertEquals(sellerInserted,sellerGathered);
+        MS.clearAll();
+    }
+
+    @Test
+    public void addingAuctionUsingObject () {
+        Seller seller = new Seller("TestSeller");
+        MS.addSeller(seller);
+        seller.setID("1111");
+        Lot lot = new Lot("Test Banana", "Turkey", "12 June 2019", "1000");
+        LS.addLOT(seller, lot);
+        lot.set_lotID(1111);
+
+        Auction auctionInserted = new Auction(seller,lot,"28 July 2019","5.5", "2");
+        MS.addAuction(auctionInserted);
+        auctionInserted.setID("1111");
+        Auction auctionGathered = MS.getAuction("1111");
+        assertEquals(auctionInserted,auctionGathered);
+        MS.clearAll();
+    }
+
 }
